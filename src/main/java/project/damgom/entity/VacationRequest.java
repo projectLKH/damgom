7855103caaa212eb9891ec3054bf52e0ff77b073
ohyoubee <dhdbql1022@gmail.com>
2023-11-09 17:10:40 +0900

    DTO 패키지 생성 휴가 신청 list.html 휴가 관련 service / Controller 생성

--- src/main/java/project/damgom/entity/VacationRequest.java
+++ src/main/java/project/damgom/entity/VacationRequest.java
@@ -17,20 +17,36 @@ public class VacationRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int vacation_request_key;
+
+
     @Column
+    //신청직원 id
     private int emp_id;
+
     @Column
+    //휴가 시작일
     private LocalDate vacation_request_start;
     @Column
+    //휴가 종료일
     private LocalDate vacation_request_end;
+
     @Column
+    //휴가의 종류
     private String vacation_request_type;
+
     @Column
+    //휴가 사유
     private String vacation_request_reason;
+
     @Column
+    //휴가 진행 상태
     private String vacation_request_status;
+
     @Column
+    //신청 시간
     private LocalDateTime vacation_request_apply_time;
+
     @Column
+    //처리 시간
     private LocalDateTime vacation_request_process_time;
 }
