265c118f2b8fcb75d18784f77ff8b67f25fb5f7d
이강현 <asan9196@naver.com>
2023-11-11 14:05:32 +0900

    string 에서 localDate로 변환 메서드 추가

--- src/main/java/project/damgom/Service/DateService.java
+++ src/main/java/project/damgom/Service/DateService.java
@@ -1,7 +1,9 @@
 package project.damgom.Service;
 
-import java.time.LocalDate;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+@Service
 public class DateService {
     public LocalDate stringTOLocalDate(String string) {
         if (string == null || string.isEmpty()) {
